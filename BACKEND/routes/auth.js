5cc8f062e77de39fc38994e1203e637e0dd53297
4K45H107 <fahad11.ahmed11@gmail.com>
2024-10-17 03:54:01 +0600

    Backend - login endpoint created

--- BACKEND/routes/auth.js
+++ BACKEND/routes/auth.js
@@ -7,9 +7,9 @@ var jwt = require("jsonwebtoken");
 
 const JWT_SECRET = "ILoveSwscgo";
 
-// create a User using: POST "/api/auth". No login required
+// ROUTE 1: CREATE a USER using: POST "/api/auth/createuser" (No login required)
 router.post(
-  "/createuser", // express-validator
+  "/createuser", // EXPRESS-VALIDATION
   [
     body("name", "Name must be atleast 3 characters").isLength({ min: 3 }),
     body("email", "Enter a valid Email").isEmail(),
@@ -18,13 +18,13 @@ router.post(
     }),
   ],
   async (req, res) => {
-    // If there are errors, return Bad request and the errors
+    // ERROR, return BAD-REQUEST and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    // Check whether the user with this email exists already
+    // user with this "email" ALREADY EXISTS
     try {
       let user = await User.findOne({ email: req.body.email });
       if (user) {
@@ -33,18 +33,63 @@ router.post(
           .json({ error: "Sorry a user with this email already exists" });
       }
 
-      // hashing the password
+      // PASSWORD-HASHING
       const salt = await bcrypt.genSalt(10);
       const secPass = await bcrypt.hash(req.body.password, salt);
 
-      // create a new user
+      // CREATE a new USER
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
         password: secPass,
       });
 
-      // jwt authentication
+      // JWT-AUTHENTICATION
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+// ROUTE 2: AUTHENTICATE a USER using: POST "/api/auth/login"
+router.post(
+  "/login", // EXPRESS-VALIDATION
+  [
+    body("email", "Enter a valid Email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    // ERROR, return BAD-REQUEST and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please try to login with correct credentials" });
+      }
+
+      // JWT-PAYLOAD
       const data = {
         user: {
           id: user.id,
